ccf9ff6fc5fcde467b8e6ad238d210974cd68ad9
neversometimes <128316603+neversometimes@users.noreply.github.com>
2024-07-17 11:35:56 -0700

    1) updated pom.xml versions 2) removed deprecated @JsonTestCase and replaced with @Scenario 3) added consumer test in orderMessages.json to validate message content

--- src/test/java/ProducerTest.java
+++ src/test/java/ProducerTest.java
@@ -1,6 +1,6 @@
 // ProducerTest.java
 
-import org.jsmart.zerocode.core.domain.JsonTestCase;
+import org.jsmart.zerocode.core.domain.Scenario;
 import org.jsmart.zerocode.core.domain.TargetEnv;
 import org.jsmart.zerocode.core.runner.ZeroCodeUnitRunner;
 import org.junit.Test;
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
 public class ProducerTest {
 
     @Test
-    @JsonTestCase ("test_cases/orderMessages.json")
+    @Scenario("test_cases/orderMessages.json")  //JSON tests file
     public void verifySuccessfulCreationOfOrderDetailsMessageInBroker() throws Exception {
 
     }
